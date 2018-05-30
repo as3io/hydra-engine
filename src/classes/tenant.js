022011394c3b022934cc2743c3aea2ca274a3870
Josh Worden <solocommand@gmail.com>
2018-05-30 16:17:20 -0500

    load org/project asynchronously

--- src/classes/tenant.js
+++ src/classes/tenant.js
@@ -1,7 +1,22 @@
+const OrganizationRepo = require('../repositories/organization');
+const ProjectRepo = require('../repositories/project');
+
 class Tenant {
   constructor({ organizationId, projectId } = {}) {
     this.organizationId = organizationId;
     this.projectId = projectId;
+  }
+
+  getOrganization() {
+    if (this.organization) return this.organization;
+    this.organization = OrganizationRepo.findById(this.organizationId);
+    return this.organization;
+  }
+
+  getProject() {
+    if (this.project) return this.project;
+    this.project = ProjectRepo.findById(this.projectId);
+    return this.project;
   }
 }
 
