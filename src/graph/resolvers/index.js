c7d5ff683b2f55344c826e791f78aef9acb32ab6
Jacob Bare <jacob@limit0.io>
2018-06-01 11:08:32 -0500

    Remove root content graph defs/resolvers

--- src/graph/resolvers/index.js
+++ src/graph/resolvers/index.js
@@ -4,13 +4,11 @@ const { DateType, CursorType } = require('@limit0/graphql-custom-types');
 const user = require('./user');
 const organization = require('./organization');
 const project = require('./project');
-const content = require('./content');
 
 module.exports = deepAssign(
   user,
   organization,
   project,
-  content,
   {
     /**
      *
