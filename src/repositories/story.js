2d7fed7ddc624f5276c788b1304c1117dbf8b946
Jacob Bare <jacob@limit0.io>
2018-06-01 12:01:05 -0500

    Check for projectId

--- src/repositories/story.js
+++ src/repositories/story.js
@@ -30,6 +30,7 @@ module.exports = {
     published,
   } = {}) {
     if (!id) throw new Error('Unable to update story: no ID was provided.');
+    if (!projectId) throw new Error('Unable to update story: no project ID was provided.');
     const story = await this.findOne({ _id: id, projectId });
     if (!story) throw new Error(`Unable to update story: no story was found in project for ID "${id}"`);
     story.set({
