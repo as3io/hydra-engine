f97644f5d9c31c3c936ab9f0e28f409328730303
Jacob Bare <jacob@limit0.io>
2018-06-04 12:19:01 -0500

    Add index and token model tests

---
+++ test/models/token.spec.js
@@ -0,0 +1,35 @@
+const Token = require('../../src/models/token');
+const { testUniqueField, testRequiredField, testTrimmedField } = require('./utils');
+
+const generateToken = () => new Token({ action: 'some-action', payload: { jti: '1234' } });
+
+describe('models/token', function() {
+  let token;
+  beforeEach(function() {
+    token = generateToken();
+  });
+
+  afterEach(async function() {
+    await Token.remove();
+  });
+
+  describe('.action', function() {
+    ['', ' ', null, undefined].forEach((value) => {
+      it(`should be required and be rejected when the value is '${value}'`, function() {
+        return testRequiredField(Token, token, 'action', value);
+      });
+    });
+    it('should be trimmed.', function() {
+      return testTrimmedField(Token, token, 'action');
+    });
+  });
+
+  describe('.payload', function() {
+    describe('.jti', function() {
+      it('should be unique', async function() {
+        const another = generateToken();
+        await testUniqueField(Token, token, another, 'payload.jti');
+      });
+    });
+  });
+});
