b0f0055004a17979f5ab0e09fc86892555829cdf
Jacob Bare <jacob@limit0.io>
2018-06-04 14:47:29 -0500

    Add initial org member model tests

--- test/models/organization-member.spec.js
+++ test/models/organization-member.spec.js
@@ -1,3 +1,63 @@
+require('../connections');
+const bcrypt = require('bcrypt');
+const OrganizationMember = require('../../src/models/organization-member');
+const Organization = require('../../src/models/organization');
+const Project = require('../../src/models/project');
+const User = require('../../src/models/user');
+const Seed = require('../../src/fixtures/seed');
+const {  testRequiredField, testRefOne } = require('./utils');
+
+const sandbox = sinon.createSandbox();
+
 describe('models/organization-member', function() {
+  before(async function() {
+    sandbox.stub(bcrypt, 'hash').resolves('$2a$04$jdkrJXkU92FIF4NcprNKWOcMKoOG28ELDrW2HBpDZFSmY/vxOj4VW');
+    await Promise.all([
+      Organization.remove(),
+      Project.remove(),
+      User.remove(),
+    ]);
+  });
+
+  after(async function() {
+    sandbox.restore();
+    await Promise.all([
+      Organization.remove(),
+      Project.remove(),
+      User.remove(),
+    ]);
+  });
+
+  let member;
+  beforeEach(async function() {
+    member = await Seed.organizationMembers(1);
+  });
+  afterEach(async function() {
+    await OrganizationMember.remove();
+  });
+
+  describe('.userId', async () => {
+    [null, undefined].forEach((value) => {
+      it(`should be required and be rejected when the value is '${value}'.`, function() {
+        return testRequiredField(member, 'userId', value);
+      });
+    });
+    it('should reject when the organization ID cannot be found.', function() {
+      const id = '5b1050d68dd51b05976c9dbf';
+      return testRefOne(member, 'userId', id, `No user found for ID ${id}`);
+    });
+  });
+
+  describe('.organizationId', async () => {
+    [null, undefined].forEach((value) => {
+      it(`should be required and be rejected when the value is '${value}'.`, function() {
+        return testRequiredField(member, 'organizationId', value);
+      });
+    });
+    it('should reject when the organization ID cannot be found.', function() {
+      const id = '5b1050d68dd51b05976c9dbf';
+      return testRefOne(member, 'organizationId', id, `No organization found for ID ${id}`);
+    });
+  });
 
 });
