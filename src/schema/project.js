a691ee0f2cd2d648ab4081854c4038336e4d4382
Jacob Bare <jacob@limit0.io>
2018-06-04 13:40:29 -0500

    Update project tests

--- src/schema/project.js
+++ src/schema/project.js
@@ -1,15 +1,28 @@
 const { Schema } = require('mongoose');
+const connection = require('../connections/mongoose');
 
 const schema = new Schema({
   name: {
     type: String,
     required: true,
+    trim: true,
   },
-  description: String,
+  description: {
+    type: String,
+    trim: true,
+  },
   organizationId: {
     type: Schema.Types.ObjectId,
     ref: 'organization',
     required: true,
+    validate: {
+      async validator(v) {
+        const doc = await connection.model('organization').findOne({ _id: v }, { _id: 1 });
+        if (doc) return true;
+        return false;
+      },
+      message: 'No organization found for ID {VALUE}',
+    },
   },
 }, { timestamps: true });
 
---
+++ test/models/project.spec.js
@@ -0,0 +1,65 @@
+require('../connections');
+const Organization = require('../../src/models/organization');
+const Project = require('../../src/models/project');
+const fixtures = require('../../src/fixtures');
+const { testTrimmedField, testUniqueField, testRequiredField, testRefOne } = require('./utils');
+
+const generateOrganization = () => fixtures(Organization, 1).one();
+const generateProject = async () => {
+  const organization = await generateOrganization().save();
+
+  const organizationId = () => organization.id;
+  return fixtures(Project, 1, { organizationId }).one();
+};
+
+describe('schema/project', function() {
+  before(async function() {
+    await Organization.remove();
+    await Project.remove();
+  });
+
+  let project;
+  beforeEach(async function() {
+    project = await generateProject();
+  });
+
+  afterEach(async function() {
+    await Organization.remove();
+    await Project.remove();
+  });
+
+
+  it('should successfully save.', async function() {
+    await expect(project.save()).to.be.fulfilled;
+  });
+
+  describe('.name', async () => {
+    ['', ' ', null, undefined].forEach((value) => {
+      it(`should be required and be rejected when the value is '${value}'.`, function() {
+        return testRequiredField(project, 'name', value);
+      });
+    });
+    it('should be trimmed.', function() {
+      return testTrimmedField(project, 'name');
+    });
+  });
+
+  describe('.description', async () => {
+    it('should be trimmed.', function() {
+      return testTrimmedField(project, 'description');
+    });
+  });
+
+  describe('.organizationId', async () => {
+    [null, undefined].forEach((value) => {
+      it(`should be required and be rejected when the value is '${value}'.`, function() {
+        return testRequiredField(project, 'organizationId', value);
+      });
+    });
+    it('should reject when the organization ID cannot be found.', function() {
+      const id = '5b1050d68dd51b05976c9dbf';
+      return testRefOne(project, 'organizationId', id, `No organization found for ID ${id}`);
+    });
+  });
+
+});
