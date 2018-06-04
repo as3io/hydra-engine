4969ee1fab020ebf4a562c0f453cc60c9a150ae2
Jacob Bare <jacob@limit0.io>
2018-06-04 13:56:48 -0500

    Add validation to related model ids

--- src/schema/organization-member.js
+++ src/schema/organization-member.js
@@ -1,4 +1,5 @@
 const { Schema } = require('mongoose');
+const connection = require('../connections/mongoose');
 
 const role = {
   type: String,
@@ -15,6 +16,15 @@ const projectRole = new Schema({
   projectId: {
     type: Schema.Types.ObjectId,
     ref: 'project',
+    required: true,
+    validate: {
+      async validator(v) {
+        const doc = await connection.model('project').findOne({ _id: v }, { _id: 1 });
+        if (doc) return true;
+        return false;
+      },
+      message: 'No project found for ID {VALUE}',
+    },
   },
   role,
 });
@@ -24,11 +34,27 @@ const schema = new Schema({
     type: Schema.Types.ObjectId,
     ref: 'user',
     required: true,
+    validate: {
+      async validator(v) {
+        const doc = await connection.model('user').findOne({ _id: v }, { _id: 1 });
+        if (doc) return true;
+        return false;
+      },
+      message: 'No user found for ID {VALUE}',
+    },
   },
   organizationId: {
     type: Schema.Types.ObjectId,
     ref: 'organization',
     required: true,
+    validate: {
+      async validator(v) {
+        const doc = await connection.model('organization').findOne({ _id: v }, { _id: 1 });
+        if (doc) return true;
+        return false;
+      },
+      message: 'No organization found for ID {VALUE}',
+    },
   },
   role,
   invitedAt: {
