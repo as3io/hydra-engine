db4ee36776229b08fb61e5336777dbf12bfe37f3
Jacob Bare <jacob@limit0.io>
2018-06-05 14:55:04 -0500

    Add deleteSession and verifyPassword tests

--- test/services/user.spec.js
+++ test/services/user.spec.js
@@ -1,5 +1,9 @@
 require('../connections');
+const bcrypt = require('bcrypt');
 const userService = require('../../src/services/user');
+const sessionService = require('../../src/services/session');
+
+const sandbox = sinon.createSandbox();
 
 describe('services/user', function() {
   describe('#login', function() {
@@ -43,7 +47,17 @@ describe('services/user', function() {
   });
 
   describe('#deleteSession', function() {
-    it('should be tested');
+    beforeEach(async function() {
+      sandbox.spy(sessionService, 'delete');
+    });
+    afterEach(async function() {
+      sandbox.restore();
+    });
+    it('should call the session service.', async function() {
+      await userService.deleteSession('1234', '5678');
+      sandbox.assert.calledOnce(sessionService.delete);
+      sandbox.assert.calledWith(sessionService.delete, '1234', '5678');
+    });
   });
 
   describe('#checkApiCredentials', function() {
@@ -51,7 +65,18 @@ describe('services/user', function() {
   });
 
   describe('#verifyPassword', function() {
-    it('should be tested');
+    const clear = 'test-password';
+    let encoded;
+    beforeEach(async function() {
+      encoded = await bcrypt.hash(clear, 2);
+    });
+
+    it('should reject when the cleartext password is wrong.', async function() {
+      await expect(userService.verifyPassword('bad-pass', encoded)).to.be.rejectedWith(Error, 'The provided password was incorrect.');
+    });
+    it('should fulfill when the cleartext password is correct.', async function() {
+      await expect(userService.verifyPassword(clear, encoded)).to.eventually.be.true;
+    });
   });
 
   describe('#updateLoginInfo', function() {
