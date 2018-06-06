742f4196a9a14fad77783073a6dcb78be0d2a611
Jacob Bare <jacob@limit0.io>
2018-06-06 09:08:11 -0500

    Add get env util

---
+++ src/utils/get-env.js
@@ -0,0 +1,10 @@
+module.exports = {
+  get(name, required = false) {
+    const value = process.env[name];
+    if (!required) return value;
+    if (value === null || value === undefined) {
+      throw new Error(`The required environment variable '${name}' was not set.`);
+    }
+    return value;
+  },
+};
