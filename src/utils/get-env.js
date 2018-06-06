d5dc5703c165fcedb46ec867fc80ee62a587720a
Jacob Bare <jacob@limit0.io>
2018-06-06 09:13:19 -0500

    Check for empty string with required check

--- src/utils/get-env.js
+++ src/utils/get-env.js
@@ -2,7 +2,7 @@ module.exports = {
   get(name, required = false) {
     const value = process.env[name];
     if (!required) return value;
-    if (value === null || value === undefined) {
+    if (value === null || value === undefined || value === '') {
       throw new Error(`The required environment variable '${name}' was not set.`);
     }
     return value;
